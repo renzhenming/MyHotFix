0d5ccef944c1341eb98e81c0aaeb7ea16f9121a6
任振铭 <renzhenming@192.168.0.102>
2021-02-14 12:16:13 +0800

    new classloader

--- hotfix/src/main/java/com/rzm/hotfix/MyHotFix.java
+++ hotfix/src/main/java/com/rzm/hotfix/MyHotFix.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,6 +15,10 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+
+import dalvik.system.DexClassLoader;
+import dalvik.system.DexFile;
+import dalvik.system.PathClassLoader;
 
 public class MyHotFix {
 
@@ -24,14 +30,114 @@ public class MyHotFix {
      * @throws Exception
      */
     public static void fixApp(Application application) throws Exception {
-        File patchDexFile = copyFile(application, PATCH_NAME);
-        File helperDexFile = copyFile(application, HELPER_NAME);
         List<File> patchFileList = new ArrayList<>();
-        if (patchDexFile.exists()) {
-            patchFileList.add(patchDexFile);
+        File patchDexFile = copyFile(application, PATCH_NAME);
+        if (!patchDexFile.exists()) {
+            return;
         }
+        patchFileList.add(patchDexFile);
+        File helperDexFile = copyFile(application, HELPER_NAME);
         if (helperDexFile.exists()) {
             patchFileList.add(helperDexFile);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            injectNewClassLoader(application, patchFileList);
+        }
+        mergePatchDexAndAppDex(application, patchFileList);
+    }
+
+    private static void injectNewClassLoader(Application application, List<File> patchDexFileList) {
+        ClassLoader newClassLoader = createNewClassLoader(application, patchDexFileList);
+        if (newClassLoader != null && application != null) {
+            injectNewClassLoader(newClassLoader, application);
+        }
+    }
+
+    private static void injectNewClassLoader(ClassLoader newClassLoader, Application application) {
+
+    }
+
+    private static ClassLoader createNewClassLoader(Application application, List<File> patchDexFileList) {
+        if (patchDexFileList.isEmpty()) {
+            return null;
+        }
+        //1 先把补丁包的dex拼起来
+        String combinedDexPaths = null;
+        StringBuilder combinedDexPathsBuilder = new StringBuilder();
+        for (File file : patchDexFileList) {
+            //DexPathList源码中，切割pathList的时候就是通过File.pathSeparator，所以这里这样做
+            /**
+             * private static List<File> splitPaths(String searchPath, boolean directoriesOnly) {
+             *         List<File> result = new ArrayList<>();
+             *         if (searchPath != null) {
+             *             for (String path : searchPath.split(File.pathSeparator)) {
+             *                 ......
+             *                 result.add(new File(path));
+             *             }
+             *         }
+             *         return result;
+             *     }
+             */
+            ////添加:分隔符  /xx/a.dex:/xx/b.dex 注意是个":"
+            combinedDexPathsBuilder.append(file.getAbsolutePath()).append(File.pathSeparator);
+        }
+
+        //2.把apk中的dex拼起来
+
+        try {
+            //d.得到BaseDexClassLoader对象
+            ClassLoader baseDexClassLoaderObj = application.getClassLoader();
+
+            //c.从BaseDexClassLoader上得到pathList
+            Object dexPathListObj = ReflectUtils.getFieldObj(baseDexClassLoaderObj, "pathList");
+
+            //b.从DexPathList上得到dexElements
+            Object[] dexElements = (Object[]) ReflectUtils.getFieldObj(dexPathListObj, "dexElements");
+
+            String packageName = application.getPackageName();
+            for (Object elementObj : dexElements) {
+                //a.从Element中得到dexFile（有dexFile就可以得到dex路径）
+                DexFile dexFileObj = (DexFile) ReflectUtils.getFieldObj(elementObj, "dexFile");
+                String dexPath = null;
+                if (dexFileObj != null) {
+                    dexPath = dexFileObj.getName();
+                }
+                if (dexPath == null || dexPath.isEmpty()) {
+                    continue;
+                }
+                //只添加app自己的
+                if (!dexPath.contains("/" + packageName)) {
+                    continue;
+                }
+                combinedDexPathsBuilder.append(dexPath).append(File.pathSeparator);
+            }
+            combinedDexPaths = combinedDexPathsBuilder.toString();
+
+            //3.获取apk中的so加载路径
+            String combinedLibraryPaths = null;
+            List<File> nativeLibraryDirectoriesList = (List<File>) ReflectUtils.getFieldObj(dexPathListObj, "nativeLibraryDirectories");
+            StringBuilder nativeLibraryDirectoriesBuilder = new StringBuilder();
+
+            for (File file : nativeLibraryDirectoriesList) {
+                if (file == null) {
+                    continue;
+                }
+                nativeLibraryDirectoriesBuilder.append(file.getAbsolutePath()).append(File.pathSeparator);
+            }
+
+            combinedLibraryPaths = nativeLibraryDirectoriesBuilder.toString();
+
+            return new PathClassLoader(combinedDexPaths, combinedLibraryPaths, application.getClassLoader());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void mergePatchDexAndAppDex(Application application, @NonNull List<File> patchFileList) throws Exception {
+        if (patchFileList.isEmpty()) {
+            return;
         }
         ClassLoader classLoader = application.getClassLoader();
         Object dexPathListObj = ReflectUtils.getFieldObj(classLoader, "pathList");
